5aacb328cc389efc7c7b559c17843a7ca8e0d3e4
Long-Qin <mrmrmr.wayne@gmail.com>
2023-10-05 21:32:14 -0400

    Added Hilt auto injection for Denpendency indejction of constructor for various classes

--- app/src/main/java/com/laioffer/spotify/MainActivity.kt
+++ app/src/main/java/com/laioffer/spotify/MainActivity.kt
@@ -1,116 +1,52 @@
 package com.laioffer.spotify
 
 import android.os.Bundle
-import android.provider.Settings.Global
 import android.util.Log
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
-import coil.compose.AsyncImage
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.laioffer.spotify.network.NetworkApi
 import com.laioffer.spotify.network.NetworkModule
-import com.laioffer.spotify.ui.theme.SpotifyTheme
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-// customized extend AppCompatActivity
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var api: NetworkApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContent {
-//            SpotifyTheme {
-//                // A surface container using the 'background' color from the theme
-//                Surface(
-//                    modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colors.background
-//                ) {
-//                    //Greeting("Android")
-//                    AlbumCover()
-//                }
-//            }
-//        }
+
         setContentView(R.layout.activity_main)
+
         val navView = findViewById<BottomNavigationView>(R.id.nav_view)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment // cast to NavHostFragment type
-
+        // controller
+        val navHostFragment =supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
-        navController.setGraph(R.navigation.nav_graph)
 
+        // data model
+        navController.setGraph(R.navigation.nav_graph)
         NavigationUI.setupWithNavController(navView, navController)
 
-        //Networking
+        // https://stackoverflow.com/questions/70703505/navigationui-not-working-correctly-with-bottom-navigation-view-implementation
+        navView.setOnItemSelectedListener{ menuItem ->
+            NavigationUI.onNavDestinationSelected(menuItem, navController)
+            navController.popBackStack(menuItem.itemId, inclusive = false)
+            true
+        }
+
         GlobalScope.launch(Dispatchers.IO) {
-            val api = NetworkModule.provideRetrofit().create(NetworkApi::class.java)
+            // comment out
+            // val api = NetworkModule.provideRetrofit().create(NetworkApi::class.java)
             val response = api.getHomeFeed().execute().body()
             Log.d("Network", response.toString())
-        }
-    }
-}
-
-
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Composable
-private fun AlbumCover() {
-    Column {
-        Box(modifier = Modifier.size(160.dp)) {
-            AsyncImage(model = "https://upload.wikimedia.org/wikipedia/en/d/d1/Stillfantasy.jpg",
-                contentDescription = null,
-                modifier = Modifier.fillMaxSize(),
-                contentScale = ContentScale.FillBounds
-            )
-            Text(
-                text = "Still Fantasy",
-                color = Color.White,
-                modifier = Modifier
-                    .padding(bottom = 4.dp, start = 2.dp)
-                    .align(Alignment.BottomStart)
-            )
-        }
-
-        Text(
-            text = "Jay Chou",
-            modifier = Modifier.padding(top = 4.dp),
-            style = MaterialTheme.typography.body2.copy(fontWeight = FontWeight.Bold),
-            color = Color.LightGray
-        )
-    }
-}
-
-
-@Preview(showBackground = true, widthDp = 412, heightDp = 732)
-@Composable
-fun DefaultPreview() {
-    SpotifyTheme {
-        Surface {
-            AlbumCover()
         }
     }
 }
