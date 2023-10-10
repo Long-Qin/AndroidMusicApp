99a1f0b0726b229bba4f5dbccf62bb248b950bc4
Long-Qin <mrmrmr.wayne@gmail.com>
2023-10-09 21:31:10 -0400

    Created Home Fragment and ViewModel, added home screen UI with music cover and titles

--- app/src/main/java/com/laioffer/spotify/datamodel/Section.kt
+++ app/src/main/java/com/laioffer/spotify/datamodel/Section.kt
@@ -3,7 +3,7 @@ package com.laioffer.spotify.datamodel
 import com.google.gson.annotations.SerializedName
 
 data class Section(
-    @SerializedName("Section_title")
+    @SerializedName("section_title")
     val sectionTitle: String,
     val albums: List<Album>
 )
--- app/src/main/java/com/laioffer/spotify/ui/home/HomeFragment.kt
+++ app/src/main/java/com/laioffer/spotify/ui/home/HomeFragment.kt
@@ -1,18 +1,39 @@
 package com.laioffer.spotify.ui.home
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.viewModels
 import com.laioffer.spotify.R
+import dagger.hilt.android.AndroidEntryPoint
 
-class HomeFragment : Fragment() { // inherit from Fragment class and also create an constructor
+@AndroidEntryPoint
+class HomeFragment : Fragment() {
+    private val viewModel: HomeViewModel by viewModels()
+
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        // Inflate the layout for this fragment
+        return ComposeView(requireContext()).apply {
+            setContent {
+                MaterialTheme(colors = darkColors()){
+                    HomeScreen(viewModel)
+                }
+            }
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (viewModel.uiState.value.isLoading) {
+            viewModel.fetchHomeScreen()
+        }
     }
 }
