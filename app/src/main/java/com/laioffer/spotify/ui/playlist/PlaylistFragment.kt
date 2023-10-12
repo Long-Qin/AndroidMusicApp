8dcd65d4cef24daf03c6bac5d0d50e4ca80b4da8
Long-Qin <mrmrmr.wayne@gmail.com>
2023-10-11 20:07:59 -0400

    Implemented Playlist viewModel

--- app/src/main/java/com/laioffer/spotify/ui/playlist/PlaylistFragment.kt
+++ app/src/main/java/com/laioffer/spotify/ui/playlist/PlaylistFragment.kt
@@ -9,6 +9,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -16,6 +17,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class PlaylistFragment: Fragment() {
 
     private val navArgs by navArgs<PlaylistFragmentArgs>()
+    private val viewModel: PlaylistViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -34,5 +36,6 @@ class PlaylistFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         Log.d("PlaylistFragment", navArgs.album.toString())
+        viewModel.fetchPlaylist(navArgs.album)
     }
 }
